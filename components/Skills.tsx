4d8f5a1dbe3649618a26ade8cff9410f45588095
Shreya Sundar <shreyasundar27@gmail.com>
2024-03-11 14:19:12 -0700

    removed useeffect

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useEffect} from 'react'
+import React from 'react'
 import {motion} from "framer-motion"
 import { skills } from "@/lib/data"
 import { useSectionInView } from '@/lib/hooks';
