877393e13ac7013347bbdcfd9534d1f19faab632
shreyasun <shreyasundar27@gmail.com>
2024-08-07 22:23:42 -0500

    Aug 2024 update

--- lib/data.ts
+++ lib/data.ts
@@ -48,6 +48,7 @@ export const experiences = [
       "Tutor for CSE 101, Data Structures and Algorithms",
     icon: React.createElement(FaPencilAlt),
     date: "Sep 2023 - Apr 2024",
+    link: null,
   },
   {
     title: "Software Developer Intern",
@@ -67,6 +68,7 @@ export const experiences = [
       "Led UCSC's premier UI/UX Design Organization",
     icon: React.createElement(FaLightbulb),
     date: "Sep 2022 - Mar 2024",
+    link: null
   },
   {
     title: "Web Developer",
@@ -76,6 +78,7 @@ export const experiences = [
       "Contributed to development and design of several full stack applications",
     icon: React.createElement(MdComputer),
     date: "Mar 2022 - Sep 2023",
+    link: null
   },
   {
     title: "Student",
@@ -85,6 +88,7 @@ export const experiences = [
       "Recent grad from UCSC, majoring in Computer Science. Coursework: Python and C Programming, Data Structures, Algorithms, Machine Learning, Database Systems, Computer Systems Design, Web Applications, Artificial Intelligence, Software Engineering",
     icon: React.createElement(LuGraduationCap),
     date: "Sep 2021 - Jul 2024",
+    link: null
   }
 ] as const;
 
