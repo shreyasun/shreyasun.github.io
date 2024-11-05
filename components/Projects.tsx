9fdaad40c003c10a4ffc1657cf9c5518774e1550
shreyasun <shreyasundar27@gmail.com>
2024-11-05 13:38:57 -0800

    changed format of projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,63 +18,68 @@ function Projects() {
   const filteredProjects = projects.filter(project => project.category === currCategory);
 
   return (
-    <motion.section className='flex flex-col items-center justify-center gap-5' 
+    <motion.section 
+      className='flex flex-col items-center justify-center gap-5 w-[70vw] max-w-full' 
       ref={ref}
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
-      >
+    >
       <h1 className="text-xl font-bold">My Projects</h1>
       <div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'>
         {categories.map((category) => (
-          <div className={`cursor-pointer inline-block rounded-md px-3 py-1 text-sm border border-transparent hover:bg-[#bcccdc] dark:hover:bg-gray-700 mb-2 mx-auto sm:mx-0 shadow-md p-6 ${currCategory === category ? 'bg-blue3 dark:bg-blue5 text-gray-800 dark:text-gray-200' : 'bg-[#f0f4f8] dark:bg-blue4 text-gray-800 dark:text-gray-200'}`}
-          key={category} onClick={() => changeCategory(category)}>
-          {category}
-        </div>
+          <div 
+            className={`cursor-pointer inline-block rounded-md px-3 py-1 text-sm border border-transparent hover:bg-[#bcccdc] dark:hover:bg-gray-700 mb-2 mx-auto sm:mx-0 shadow-md p-6 ${currCategory === category ? 'bg-blue3 dark:bg-blue5 text-gray-800 dark:text-gray-200' : 'bg-[#f0f4f8] dark:bg-blue4 text-gray-800 dark:text-gray-200'}`}
+            key={category} 
+            onClick={() => changeCategory(category)}
+          >
+            {category}
+          </div>
         ))}
       </div>
-      
-      <div className="flex">
-        <ul className="">
-          {filteredProjects.length === 0 ? (
-            <div className='mb-4 max-w-md bg-white dark:bg-blue5 rounded-lg shadow-md p-6'>
-              <h2>Coming Soon!</h2>
-            </div>
-          ) : (
-            filteredProjects.map((project, i) => (
-              <div key={i} className='mb-4 max-w-md bg-white dark:bg-blue5 rounded-lg shadow-md p-6'>
-                <h2><b>{project.title}</b></h2>
-                <p>{project.description}</p>
-                <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-                  {project.tools.map((tool, i) => (
-                    <li
-                      className="bg-[#bcccdc]/[0.7] px-3 py-1 text-[0.7rem] mt-2 tracking-wider rounded-full dark:bg-blue4"
-                      key={i}
-                    >
-                      {tool}
-                    </li>
-                  ))}
-                </ul>
-                <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-                  {project.tags.map((tag, i) => (
-                    <li
-                      className="bg-[#5b77a4]/[0.7] px-3 py-1 text-[0.7rem] mt-2 tracking-wider rounded-full dark:bg-blue3"
-                      key={i}
-                    >
-                      {tag}
-                    </li>
-                  ))}
-                </ul>
-                <div className='cursor-pointer inline-block'>
-                  <a href={project.link} target="_blank" rel="noopener noreferrer">
-                    <div className='bg-blue3 dark:bg-blue4 mt-4 rounded-md px-3 py-1 text-sm text-gray-800 dark:text-gray-200 border border-transparent hover:bg-[#bcccdc] dark:hover:bg-gray-700 mb-2 mx-auto sm:mx-0 shadow-md flex items-center'>
-                      View Project <MdOutlineOpenInNew className="ml-1" />
-                    </div>
-                  </a>
-                </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full">
+        {filteredProjects.length === 0 ? (
+          <div className='mb-4 max-w-md bg-white dark:bg-blue5 rounded-lg shadow-md p-6'>
+            <h2>Coming Soon!</h2>
+          </div>
+        ) : (
+          filteredProjects.map((project, i) => (
+            <div 
+              key={i} 
+              className='mb-4 bg-white dark:bg-blue5 rounded-lg shadow-md p-6 flex flex-col items-start'
+            >
+              <h2><b>{project.title}</b></h2>
+              <p>{project.description}</p>
+              <ul className="flex flex-wrap mt-4 gap-2">
+                {project.tools.map((tool, i) => (
+                  <li
+                    className="bg-[#bcccdc]/[0.7] px-3 py-1 text-[0.7rem] mt-2 tracking-wider rounded-full dark:bg-blue4"
+                    key={i}
+                  >
+                    {tool}
+                  </li>
+                ))}
+              </ul>
+              <ul className="flex flex-wrap mt-4 gap-2">
+                {project.tags.map((tag, i) => (
+                  <li
+                    className="bg-[#5b77a4]/[0.7] px-3 py-1 text-[0.7rem] mt-2 tracking-wider rounded-full dark:bg-blue3"
+                    key={i}
+                  >
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+              <div className='cursor-pointer inline-block'>
+                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                  <div className='bg-blue3 dark:bg-blue4 mt-4 rounded-md px-3 py-1 text-sm text-gray-800 dark:text-gray-200 border border-transparent hover:bg-[#bcccdc] dark:hover:bg-gray-700 mb-2 mx-auto sm:mx-0 shadow-md flex items-center'>
+                    View Project <MdOutlineOpenInNew className="ml-1" />
+                  </div>
+                </a>
               </div>
-            ))
-          )}
-        </ul>
+            </div>
+          ))
+        )}
       </div>
     </motion.section>
   );
